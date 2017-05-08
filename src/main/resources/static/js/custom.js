710a54dfec9f4ce66e5810fe201b7b7804ec1557
rajesh <rajeshghosh4007@gmail.com>
2017-05-08 16:15:12 +0200

    lat lon with country dropdown done

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -7,4 +7,45 @@ $(".btn-pref .btn").click(function () {
     // $(".tab").addClass("active"); // instead of this do the below 
     $(this).removeClass("btn-default").addClass("btn-primary");   
 });
+
+
+//latitude and longitude 
+	
+var latf = $("#lat"), longf = $("#lon");
+
+$("#city").keyup(function(){
+	 var xx = $("#city").val();
+
+	var geocoder = new google.maps.Geocoder();
+	var address = xx;
+
+	geocoder.geocode( { 'address': address}, function(results, status) {
+
+	if (status == google.maps.GeocoderStatus.OK) {
+	    var latitude = results[0].geometry.location.lat();
+	    var longitude = results[0].geometry.location.lng();
+
+	     if(address =='')
+	    	{
+	    	 latf.val("");
+	 	     longf.val("");
+	    	}
+	     else
+	    	 {
+	    	 latf.val(latitude);
+	 	     longf.val(longitude);
+	    	 }
+	     
+	    
+	    
+	    } 
+	 }); 
+
+});
+
+	$("#city").keydown(function(){
+		$("#city").css("background-color", "yellow");
+	});
+	
+	
 });
